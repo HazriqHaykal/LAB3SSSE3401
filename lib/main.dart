d8c20698399c820bf7c709790ae5ad0ae157085c
HazriqHaykal <222309@student.upm.edu.my>
2025-04-10 10:25:32 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_ticket_app3/base/bottom_nav_bar.dart';
 
 void main() {
-  runApp(const MyApp()); //entry point
+
+  runApp(const MyApp());// entry point
 }
 
-class MyApp extends StatelessWidget { //stateless widget something that never change
-  const MyApp({super.key}); // A constant constructor that passes the key to the parent class (StatelessWidget)
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
       title: 'Flutter Demo',
-      theme: ThemeData(),
-      home: BottomNavBar(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 153, 110, 227)),
+      ),
+      home:  BottomNavBar()
     );
+ 
   }
 }
